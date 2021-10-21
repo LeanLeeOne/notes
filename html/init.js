074ce50310d63c9d449c6a98e654820c7f764b99
LeanLeeOne <yahaheihei@foxmail.com>
2021-10-21 19:17:32 +0800

    补充项目地址

--- html/init.js
+++ html/init.js
@@ -1,7 +1,22 @@
+const HEADER =
+  "<a href='https://github.com/LeanLeeOne/notes' target='_blank' rel='noopener noreferrer' class='git-hub-link'>" +
+  "Github" +
+  "<svg xmlns='http://www.w3.org/2000/svg' aria-hidden='true' focusable='false' x='0px' y='0px' viewBox='0 0 100 100' width='15' height='15' class='icon outbound'><path fill='currentColor' d='M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z'></path> <polygon fill='currentColor' points='45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9'></polygon></svg>" +
+  "</a>";
+const FOOTER =
+  "<div class='footer'>" +
+  "<a class='beian' href='https://beian.miit.gov.cn/' target='_blank'>鲁ICP备18052178号</a>" +
+  "<span class='copyright'>@2021</span>" +
+  "<a href='http://leanlee.top/resume/' class='resume' rel='noopener'>" +
+  "By 李晓辉" +
+  "</a>" +
+  "</div>";
+
 $(() => {
   let className = $(document.documentElement).attr("class");
   appendTitle(className);
   avoidFlashing(className);
+  appendHeader();
   appendFooter();
 });
 
@@ -14,7 +29,10 @@ function appendTitle(className) {
 
 function avoidFlashing(className) {
   // 返回时还是会执行动画
-  if(true){return};
+  if (true) {
+    return
+  }
+  ;
 
   if (className.indexOf("index_first") > -1) {
     setTimeout(() => {
@@ -33,16 +51,12 @@ function avoidFlashing(className) {
   };
 }
 
+function appendHeader() {
+  let body = $(document.body);
+  body.append(FOOTER);
+}
+
 function appendFooter() {
   let body = $(document.body);
-  body.append("\n" +
-    "    <div class=\"footer\">\n" +
-    "      <a class=\"beian\" href=\"https://beian.miit.gov.cn/\" target=\"_blank\">鲁ICP备18052178号</a>\n" +
-    "      <span class=\"copyright\">@2021</span>\n" +
-    "      <a href=\"http://leanlee.top/resume/\"\n" +
-    "         class=\"resume\"\n" +
-    "         rel=\"noopener\">\n" +
-    "        By 李晓辉\n" +
-    "      </a>\n" +
-    "    </div>")
+  body.append(FOOTER);
 }
